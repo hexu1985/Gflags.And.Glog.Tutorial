d3e9b28ff28e5d5d0b8b27caf5cdf8b8f9d0cfe4
hexu <hexu_1985@sina.com>
2022-04-06 11:27:05 +0800

    update build.sh

--- chapter-01/recipe-02/build.sh
+++ chapter-01/recipe-02/build.sh
@@ -1,4 +1,4 @@
-cmake -H. -Bbuild -DCMAKE_INSTALL_PREFIX=~/local \
-    -Dgflags_DIR=~/local/lib/cmake/gflags/ \
-    -DBUILD_SHARED_LIBS=OFF 
+cmake -H. -Bbuild -DCMAKE_INSTALL_PREFIX=~/local/glog \
+    -Dgflags_DIR=~/local/gflag/lib/cmake/gflags/
+#    -DBUILD_SHARED_LIBS=OFF 
 cmake --build build --target install
