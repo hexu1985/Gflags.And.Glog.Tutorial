eded6263ec7b28c0aa145d68cd0314f0965d72b1
hexu <hexu_1985@sina.com>
2022-09-13 18:56:32 +0800

    update chapter-03/recipe-01

--- chapter-03/recipe-01/src/test.sh
+++ chapter-03/recipe-01/src/test.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
+export LD_LIBRARY_PATH=~/local/gflags/lib
+
 echo "./sample"
+./sample
+
+echo
+echo
+
+echo "GLOG_log_dir=. GLOG_alsologtostderr=true ./sample"
 GLOG_log_dir=. GLOG_alsologtostderr=true ./sample
