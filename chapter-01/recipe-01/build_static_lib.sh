014797205a9419e867d45bca452fd4e2ac88b529
hexu <hexu_1985@sina.com>
2022-09-13 15:52:24 +0800

    update gflags/build*.sh

---
+++ chapter-01/recipe-01/build_static_lib.sh
@@ -0,0 +1,8 @@
+cmake -H. -Bbuild -DCMAKE_INSTALL_PREFIX=~/local/gflags \
+	-DBUILD_gflags_LIB=ON \
+	-DBUILD_STATIC_LIBS=ON \
+    -DBUILD_SHARED_LIBS=OFF \
+	-DINSTALL_STATIC_LIBS=ON \
+	-DINSTALL_SHARED_LIBS=OFF \
+	-DREGISTER_INSTALL_PREFIX=OFF 
+cmake --build build --target install
