198f3fe1cebf311e9438dd81c4fdeb72bb896b9b
hexu <hexu_1985@sina.com>
2022-09-13 18:45:26 +0800

    update glog/build*.sh

--- chapter-01/recipe-02/build_dbg.sh
+++ chapter-01/recipe-02/build_dbg.sh
@@ -1,5 +1,5 @@
 cmake -H. -Bbuild -DCMAKE_INSTALL_PREFIX=~/local/glog_dbg \
     -DCMAKE_BUILD_TYPE=Debug \
-    -Dgflags_DIR=~/local/gflag_dbg/lib/cmake/gflags/ \
+    -Dgflags_DIR=~/local/gflags_dbg/lib/cmake/gflags/ \
     -DBUILD_SHARED_LIBS=OFF
 cmake --build build --target install
--- chapter-01/recipe-02/build_shared_lib.sh
+++ chapter-01/recipe-02/build_shared_lib.sh
@@ -1,4 +1,4 @@
 cmake -H. -Bbuild -DCMAKE_INSTALL_PREFIX=~/local/glog \
-    -Dgflags_DIR=~/local/gflag/lib/cmake/gflags/ \
+    -Dgflags_DIR=~/local/gflags/lib/cmake/gflags/ \
     -DBUILD_SHARED_LIBS=ON
 cmake --build build --target install
