4e2b132567f00643a09ad690ab987205b4e48cfd
hexu <hexu_1985@sina.com>
2025-02-15 09:05:49 +0800

    update chapter-01/recipe-01/build_static_lib.sh

--- chapter-01/recipe-01/build_static_lib.sh
+++ chapter-01/recipe-01/build_static_lib.sh
@@ -1,8 +1,9 @@
-cmake -H. -Bbuild -DCMAKE_INSTALL_PREFIX=~/local/gflags \
+cmake -H. -Bbuild \
 	-DBUILD_gflags_LIB=ON \
 	-DBUILD_STATIC_LIBS=ON \
     -DBUILD_SHARED_LIBS=OFF \
 	-DINSTALL_STATIC_LIBS=ON \
 	-DINSTALL_SHARED_LIBS=OFF \
 	-DREGISTER_INSTALL_PREFIX=OFF 
-cmake --build build --target install
+cmake --build build --parallel 4 
+cmake --install build --prefix ~/local/gflags
